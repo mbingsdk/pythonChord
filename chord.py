23ce9d7cb1121553f3ff5ad5495f03996059fee4
Tamin Robelcom <53684389+mbingsdk@users.noreply.github.com>
2021-11-14 18:22:35 +0800

    Update chord.py

--- chord.py
+++ chord.py
@@ -3,7 +3,7 @@ import enquiries
 
 lagu = {}
 test = input("\nPenyanyi: ")
-out = getChord("getList", link, test, head)
+out = getChord("getList", test)
 
 for i in range(len(out)):
     lagu[out[i][2]] = str(out[i][0])
@@ -11,7 +11,7 @@ for i in range(len(out)):
 choice = enquiries.choose("\nLagu "+out[0][1], list(lagu))
 print(choice, "\n")
 
-ret = getChord("getChord", link, lagu[choice], head)
+ret = getChord("getChord", lagu[choice])
 penyanyi = ret[0][0]
 judul = ret[0][1]
 chordLagu = ret[0][2]
