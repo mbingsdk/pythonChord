684812317a6bd77f0f8b0d3bd217f97f9e4a763f
Tamin Robelcom <53684389+mbingsdk@users.noreply.github.com>
2021-11-15 22:32:10 +0800

    Update chord.py

--- chord.py
+++ chord.py
@@ -1,21 +1,6 @@
+#chord main
 from func import *
-import enquiries
 
-lagu = {}
 test = input("\nPenyanyi: ")
-out = getChord("getList", test)
-
-for i in range(len(out)):
-    lagu[out[i][2]] = str(out[i][0])
-
-choice = enquiries.choose("\nLagu "+out[0][1], list(lagu))
-print(choice, "\n")
-
-ret = getChord("getChord", lagu[choice])
-penyanyi = ret[0][0]
-judul = ret[0][1]
-chordLagu = ret[0][2]
-
-result = rebuild(chordLagu)
-print(result)
-#print(chordLagu)
+r = menu(test)
+showChord(r[0], r[1])
