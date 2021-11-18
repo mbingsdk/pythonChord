325f8f41ea06128a045f48aed3cfc6396a5b1697
Tamin Robelcom <53684389+mbingsdk@users.noreply.github.com>
2021-11-18 23:17:10 +0800

    Update chord.py

--- chord.py
+++ chord.py
@@ -1,12 +1,5 @@
 #chord main
 from func import *
 
-while True:
-    print("\nKetik !exit untuk keluar")
-    test = input("\nPenyanyi: ")
-    if test == "!exit":
-        exit()
-    else:
-        a = chord()
-        a.menu(test)
-        a.showChord()
+a = chord()
+a.run()
